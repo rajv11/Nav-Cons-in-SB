48b7dd5ab774d6b451047f45580772d1e4dd0349
student <student@nw43732.nwmissouri.edu>
2018-10-05 12:41:57 -0500

    Initial Commit

---
+++ Nav Cons in SB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nav Cons in SB
+//
+//  Created by student on 10/5/18.
+//  Copyright © 2018 navs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
